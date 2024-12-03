fac91336d0c44226aead5bf463628410f59f1dc0
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-12-03 20:37:17 +0600

    feat: Implement routing configuration
    - Set up routing structure with createBrowserRouter
    - Integrated MainLayout as the main layout component
    - Added Home, AllMovies, AddMovie, UpdateMovie, MovieDetails, MyFavorites, Login, Register, and NotFound pages
    - Implemented PrivateRoute for protected routes: AddMovie, UpdateMovie, MyFavorites
    - Defined nested routes under MainLayout with error handling via NotFound component

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,68 @@
-const Routes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../layouts/MainLayout';
+import Home from '../pages/Home';
+import AllMovies from '../pages/AllMovies';
+import AddMovie from '../pages/AddMovie';
+import UpdateMovie from '../pages/UpdateMovie';
+import MovieDetails from '../pages/MovieDetails';
+import MyFavorites from '../pages/MyFavorites';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import NotFound from '../pages/NotFound';
+import PrivateRoute from './PrivateRoute';
 
-export default Routes;
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/movies',
+                element: <AllMovies />
+            },
+            {
+                path: '/movies/:id',
+                element: <MovieDetails />
+            },
+            {
+                path: '/add-movie',
+                element: (
+                    <PrivateRoute>
+                        <AddMovie />
+                    </PrivateRoute>
+                )
+            },
+            {
+                path: '/update-movie/:id',
+                element: (
+                    <PrivateRoute>
+                        <UpdateMovie />
+                    </PrivateRoute>
+                )
+            },
+            {
+                path: '/favorites',
+                element: (
+                    <PrivateRoute>
+                        <MyFavorites />
+                    </PrivateRoute>
+                )
+            },
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/register',
+                element: <Register />
+            }
+        ]
+    }
+]);
+
+export default router;

db1fe9538039eda11959233ba26058bac5be205b
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-12-03 21:45:31 +0600

    feat: Add ForgotPassword page with form for password reset
    - Created ForgotPassword page with a form for users to reset their password
    - Added input field for email with appropriate styles and validation
    - Included link to navigate back to the login page if the user remembers their password
    - Styled the form and button for a consistent user experience
    - Implemented form submission handling

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,8 +1,39 @@
+import { Link } from "react-router-dom";
+
 const ForgotPassword = () => {
+    const handleForgetPasswordForm = e => {
+        e.preventDefault();
+    };
+
     return (
-        <div>
-            <h1>Forgot Password Page</h1>
-        </div>
+        <section className="bg-gray-50">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                            Forgot password?
+                        </h1>
+                        <p className="text-sm font-light text-gray-500">
+                            Remember your password? <Link to={"/login"} className="font-medium text-[#E50916] hover:underline">Login here</Link>
+                        </p>
+                        <form onSubmit={handleForgetPasswordForm} className="space-y-4 md:space-y-6" action="#">
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    placeholder="name@company.com"
+                                    required
+                                />
+                            </div>
+                            <button type="submit" className="w-full text-white bg-[#E50916] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center">Reset Password</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 

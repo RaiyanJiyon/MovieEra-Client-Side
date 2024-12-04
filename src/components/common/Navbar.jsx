8b337e03fac6b7c46bbb5a547081d5a9c3ed8012
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-12-04 08:56:44 +0600

    feat: Add PrivateRoute component to protect authenticated routes
    - Implement PrivateRoute component in the routes folder
    - Use authContext to check user authentication and loading state
    - Display loading spinner while authentication state is being determined
    - Redirect unauthenticated users to the login page, preserving the intended path

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -92,7 +92,10 @@ const Navbar = () => {
                             </ul>
                         </div>
                         :
-                        <Link to={"/login"} className="px-4 py-1 rounded-3xl bg-black text-white font-semibold border-2 border-[#E50916] hover:bg-white hover:text-black">Login</Link>
+                        <div className="space-x-2">
+                        <Link to={"/login"} className="px-4 py-1 rounded-3xl bg-black text-white font-semibold border-2 border-[#F15E75] hover:bg-white hover:text-black">Login</Link>
+                        <Link to={"/register"} className="px-4 py-1 rounded-3xl bg-black text-white font-semibold border-2 border-[#F15E75] hover:bg-white hover:text-black">Register</Link>
+                        </div>
                 }
             </div>
         </div >
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,9 +1,22 @@
-const PrivateRoute = () => {
-    return (
-        <div>
-            
+import { useContext } from "react";
+import { authContext } from "../contexts/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(authContext);
+    const location = useLocation();
+
+    if (loading) {
+        <div className="flex justify-center items-center h-screen">
+            <span className="loading loading-dots loading-lg text-[#E50916]"></span>
         </div>
-    );
+    };
+
+    if (user) {
+        return children;
+    };
+
+    return <Navigate to={"/login"} state={location.pathname} replace />
 };
 
 export default PrivateRoute;

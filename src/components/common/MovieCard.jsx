097602d06792232e8a4032406f201e5a657652b9
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-12-08 12:21:10 +0600

    feat: Add MovieCard component and integrate with AddMovie and FeaturedMovies
    - Implemented MovieCard component to display movie details including poster, title, genre, duration, release year, rating, and summary
    - Utilized PropTypes to enforce type checking for movie prop
    - Integrated MovieCard component with AddMovie and FeaturedMovies components
    - Applied responsive design and hover effects using Tailwind CSS
    - Improved code reusability and maintainability by encapsulating movie card UI in a dedicated component

--- src/components/common/MovieCard.jsx
+++ src/components/common/MovieCard.jsx
@@ -1,9 +1,46 @@
-const MovieCard = () => {
+import { FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types'; // ES6
+
+const MovieCard = ({ movie }) => {
     return (
-        <div>
-            
-        </div>
+            <div className="card card-compact text-white shadow-lg rounded-lg overflow-hidden">
+                <figure>
+                    <img
+                        className="w-full h-[300px] object-cover lg:object-fill transition-transform duration-500 ease-in-out transform hover:scale-110"
+                        src={movie.moviePoster}
+                        alt={`${movie.movieTitle} poster`}
+                    />
+                </figure>
+                <div className="card-body p-4 text-black">
+                    <h2 className="card-title text-black text-2xl font-bold mb-2 ">{movie.movieTitle}</h2>
+                    <div className="flex justify-start items-center">
+                        <div className="flex items-center gap-2">
+                            <FaStar className="text-orange-400" />
+                            <p className="text-gray-400">{`${movie.rating}/5`}</p>
+                        </div>
+                        <div className="text-sm text-gray-400 ml-14">
+                            {Array.isArray(movie.genre)
+                                ? movie.genre.join(', ')
+                                : movie.genre
+                            }
+                        </div>
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <p className="text-sm text-gray-400 mb-2">Duration: {movie.duration} mins</p>
+                        <p className="text-sm text-gray-400 mb-2">Release Year: {movie.releaseYear}</p>
+                    </div>
+                    <p className="text-sm text-gray-400 mb-2">{movie.summary}</p>
+                    <Link to={`/movies/${movie._id}`} className="card-actions justify-start mt-4">
+                        <button className="btn bg-[#2ce6e6] text-black font-semibold">See Details</button>
+                    </Link>
+                </div>
+            </div>
     );
 };
+
+MovieCard.propTypes = {
+    movie: PropTypes.object.isRequired
+}
 
 export default MovieCard;
--- src/components/home/FeaturedMovies.jsx
+++ src/components/home/FeaturedMovies.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import MovieCard from "../common/MovieCard";
 
 const FeaturedMovies = () => {
     const [movies, setMovies] = useState([]);
@@ -28,37 +29,7 @@ const FeaturedMovies = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-10">
                 {
                     movies.map((movie, idx) => (
-                        <div key={idx} className="card card-compact text-white shadow-lg rounded-lg overflow-hidden">
-                            <figure>
-                                <img
-                                    className="w-full h-[300px] object-cover lg:object-fill transition-transform duration-500 ease-in-out transform hover:scale-110"
-                                    src={movie.moviePoster}
-                                    alt={`${movie.movieTitle} poster`} />
-                            </figure>
-                            <div className="card-body p-4 text-black">
-                                <h2 className="card-title text-black text-2xl font-bold mb-2">{movie.movieTitle}</h2>
-                                <div className="flex justify-start items-center">
-                                    <div className="flex items-center gap-2">
-                                        <FaStar className="text-orange-400" />
-                                        <p className="text-gray-400">{`${movie.rating}/5`}</p>
-                                    </div>
-                                    <div className="text-sm text-gray-400 ml-14">
-                                    {Array.isArray(movie.genre) 
-                                        ? movie.genre.join(', ')
-                                        : movie.genre
-                                    }
-                                </div>
-                                </div>
-                                <div className="flex justify-between items-center">
-                                    <p className="text-sm text-gray-400 mb-2">Duration: {movie.duration} mins</p>
-                                    <p className="text-sm text-gray-400 mb-2">Release Year: {movie.releaseYear}</p>
-                                </div>
-                                <p className="text-sm text-gray-400 mb-2">{movie.summary}</p>
-                                <Link to={`/movies/${movie._id}`} className="card-actions justify-start mt-4">
-                                    <button className="btn bg-[#2ce6e6] text-black font-semibold">See Details</button>
-                                </Link>
-                            </div>
-                        </div>
+                        <MovieCard key={idx} movie={movie} />
                     ))
                 }
             </div>
